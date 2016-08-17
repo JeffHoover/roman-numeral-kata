6d2a22fc2d0981a0cbcc014814b9db82fcbe77c5
Jeff Hoover <jhoover@jhoover.com>
2016-08-16 22:25:40 -0400

    Used correct form of libcheck's Looping Tests instead of a for loop in tests.

--- tests/test_roman_numbers.c
+++ tests/test_roman_numbers.c
@@ -9,17 +9,20 @@
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+#define VALID_DATA_COUNT 41 
 
 // If I wanted to spend more time,
 // I would make it easier to eyeball the arabic/roman pairs. That might be a big-ish refactor.
 // The way I am leaving it, the 4 rows of arrray values map 1:1 between arabic and roman, which isn't too bad.
-int arabic_values [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-                        16, 17, 18, 19, 20, 24, 40, 47, 50, 67, 74, 90, 99,
+int arabic_values [VALID_DATA_COUNT] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+                        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+                        24, 40, 47, 50, 67, 74, 90, 99,
                         100, 202, 330, 400, 482, 500, 900, 1000,
                         2001, 3030, 4400, 4894, 4999};
 
-char * roman_values [] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV",
-                          "XVI", "XVII", "XVIII", "XIX", "XX", "XXIV", "XL", "XLVII", "L", "LXVII", "LXXIV", "XC", "XCIX",
+char * roman_values [VALID_DATA_COUNT] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                                          "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", 
+                         "XXIV", "XL", "XLVII", "L", "LXVII", "LXXIV", "XC", "XCIX",
                           "C","CCII", "CCCXXX", "CD", "CDLXXXII", "CDC", "CM", "M",
                           "MMI", "MMMXXX", "MMMMCD", "MMMMCDCDXCIV", "MMMMCMXCIX"};
 
@@ -53,58 +56,49 @@ START_TEST (addition)
      char * rightAddend = "I";
      char sumResult [20]; // This might be a few bytes too big. I'm ok with that.
 
+     printf("\n");
      add (leftAddend, rightAddend, (char *) sumResult);
 
      ck_assert_msg(strcmp(expectedSum, sumResult) == 0,
                  ANSI_COLOR_RED
-                 "\n%s + %s expected %s but was %s\n"
+                 "%s + %s expected %s but was %s\n"
                  ANSI_COLOR_RESET,
                  leftAddend, rightAddend, expectedSum, sumResult);
 
      printf(ANSI_COLOR_GREEN "%s + %s = %s\n" ANSI_COLOR_RESET, leftAddend, rightAddend, sumResult);
-     printf("\n");
 }
 END_TEST
+
 
 START_TEST (to_roman)
 {
     char romanResult[15]; // This might be a few bytes too big. I'm ok with that.
                           // Longest roman numeral in test is "MMMMDCCCXCIV"
 
-    for (unsigned int ii=0; ii< sizeof(arabic_values)/sizeof(int); ii++)
-    {
-        arabic_to_roman(arabic_values[ii], romanResult);
+    arabic_to_roman(arabic_values[_i], romanResult);
 
-        ck_assert_msg(strcmp(romanResult, roman_values[ii])==0,
-            ANSI_COLOR_RED
-            "\nconvert_arabic_to_roman(%d) expected %s but was %s\n"
-                 ANSI_COLOR_RESET,
-                 arabic_values[ii], roman_values[ii], romanResult);
+    ck_assert_msg(strcmp(romanResult, roman_values[_i])==0,
+        ANSI_COLOR_RED
+        "convert_arabic_to_roman(%d) expected %s but was %s\n"
+             ANSI_COLOR_RESET,
+             arabic_values[_i], roman_values[_i], romanResult);
 
-         printf(ANSI_COLOR_GREEN "convert_arabic_to_roman(%d) = %s\n" ANSI_COLOR_RESET ,
-                arabic_values[ii], romanResult);
-     }
-
-     printf("\n");
+     printf(ANSI_COLOR_GREEN "convert_arabic_to_roman(%d) = %s\n" ANSI_COLOR_RESET ,
+            arabic_values[_i], romanResult);
 }
 END_TEST
 
 START_TEST (to_arabic)
 {
-     printf("\n");
-     for (unsigned int ii=0; ii < sizeof(arabic_values)/sizeof(int); ii++)
-     {
-         int arabicValue = roman_to_arabic(roman_values[ii]);
+     int arabicValue = roman_to_arabic(roman_values[_i]);
 
-         ck_assert_msg(arabicValue == arabic_values[ii],
-                 ANSI_COLOR_RED
-                 "\nconvert_roman_to_arabic(%s) expected %d but was %d\n"
-                 ANSI_COLOR_RESET,
-                 roman_values[ii], arabic_values[ii], arabicValue);
-         printf(ANSI_COLOR_GREEN "convert_roman_to_arabic(%s) = %d\n" ANSI_COLOR_RESET, roman_values[ii], arabicValue); 
-     }
+     ck_assert_msg(arabicValue == arabic_values[_i],
+             ANSI_COLOR_RED
+             "convert_roman_to_arabic(%s) expected %d but was %d\n"
+             ANSI_COLOR_RESET,
+             roman_values[_i], arabic_values[_i], arabicValue);
+     printf(ANSI_COLOR_GREEN "convert_roman_to_arabic(%s) = %d\n" ANSI_COLOR_RESET, roman_values[_i], arabicValue); 
 
-     printf("\n");
 }
 END_TEST
 
@@ -115,10 +109,11 @@ Suite * roman_suite (void)
   Suite *s = suite_create ("Roman Numeral Unit Tests");
 
   TCase *tc_to_arabic = tcase_create ("RomanToArabic");
-  tcase_add_test (tc_to_arabic, to_arabic);
+  tcase_add_loop_test(tc_to_arabic, to_arabic, 0, VALID_DATA_COUNT);
   suite_add_tcase (s, tc_to_arabic);
 
   TCase *tc_to_roman = tcase_create ("ArabicToRoman");
+  tcase_add_loop_test(tc_to_roman, to_roman, 0, VALID_DATA_COUNT);
   tcase_add_test (tc_to_roman, to_roman);
   suite_add_tcase (s, tc_to_roman);
 
