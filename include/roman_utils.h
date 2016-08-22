06645ca93ce89f008bea045879fe29d7bf94bc6c
Jeff Hoover <jhoover@jhoover.com>
2016-08-21 22:53:57 -0400

    adding const again

--- include/roman_utils.h
+++ include/roman_utils.h
@@ -1,6 +1,6 @@
 #ifndef ROMAN_UTILS_H
 #define ROMAN_UTILS_H
-void add (char * leftAddend, char * rightAddend, char * sum);
-void subtract (char * minuend, char * subtrahend, char * differenceResult);
+void add (const char * leftAddend, const char * rightAddend, char * sum);
+void subtract (const char * minuend, const char * subtrahend, char * differenceResult);
 
 #endif /* ROMAN_UTILS_H */
--- src/roman_utils.c
+++ src/roman_utils.c
@@ -2,7 +2,7 @@
 #include <arabic_to_roman.h>
 #include <roman_to_arabic.h>
 
-void add(char *leftAddend, char *rightAddend, char *sum)
+void add(const char *leftAddend, const char *rightAddend, char *sum)
 {
     // Originally, I didn't inline the roman_to_arabic calls below. I think I like this better.
     // (Same in subtract function below.)
@@ -10,7 +10,7 @@ void add(char *leftAddend, char *rightAddend, char *sum)
 		    sum);
 }
 
-void subtract(char *minuend, char *subtrahend, char *differenceResult)
+void subtract(const char *minuend, const char *subtrahend, char *differenceResult)
 {
     arabic_to_roman(roman_to_arabic(minuend) - roman_to_arabic(subtrahend),
 		    differenceResult);
